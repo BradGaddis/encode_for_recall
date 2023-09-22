31a78a92e53768e60b777b23e91a1234c976a038
Brad Gaddis <bradleygad@gmail.com>
2023-09-22 16:20:06 -0400

    clean up names and methods

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # encode_for_recall
-A plugin for Obisidian that replaces characters for better recall
+
+This a plugin for Obsidian that replaces characters for better recall. It is based on the [Encoding for Recall](https://www.supermemo.com/en/archives1990-2015/english/ol/sm2) method by P.A. Wozniak.
+
+## The PDF can be found here:
+https://link.springer.com/content/pdf/10.3758/BF03196974.pdf
+
+At present, we are only replacing character from nouns. You can choose the replacement from the plugin settings
--- main.ts
+++ main.ts
@@ -1,35 +1,23 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Editor, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+interface EncoderRecallSettings {
+	replacementChar: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: EncoderRecallSettings = {
+	replacementChar: 'default'
 }
 
-function replaceText(editor: Editor) {
-	const text = editor.getValue();
-  
-	// Replace text here
-	const newText = text.replace(/old text/g, 'new text');
-  
-	editor.setValue(newText);
-	// editor.save();
-  }
-
-export default class Encoder extends Plugin {
-	settings: MyPluginSettings;
+export default class EncoderRecall extends Plugin {
+	settings: EncoderRecallSettings;
 
 	async onload() {
 		await this.loadSettings();
 
 		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
+		const ribbonIconEl = this.addRibbonIcon('dice', 'Encode Text', (evt: MouseEvent) => {
 			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
+			new Notice('Text encoded!');
 		});
 		// Perform additional things with the ribbon
 		ribbonIconEl.addClass('my-plugin-ribbon-class');
@@ -38,45 +26,8 @@ export default class Encoder extends Plugin {
 		const statusBarItemEl = this.addStatusBarItem();
 		statusBarItemEl.setText('Status Bar Text');
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
 		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+		this.addSettingTab(new EncodingRecallSettings(this.app, this));
 
 		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
 		// Using this function will automatically remove the event listener when this plugin is disabled.
@@ -100,27 +51,10 @@ export default class Encoder extends Plugin {
 		await this.saveData(this.settings);
 	}
 }
+class EncodingRecallSettings extends PluginSettingTab {
+	plugin: EncoderRecall;
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: Encoder;
-
-	constructor(app: App, plugin: Encoder) {
+	constructor(app: App, plugin: EncoderRecall) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
@@ -133,13 +67,13 @@ class SampleSettingTab extends PluginSettingTab {
 		containerEl.empty();
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
+			.setName('Replacement Character')
+			.setDesc('The character that will replace the original character during encoding')
 			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+				.setPlaceholder('Enter a character')
+				.setValue(this.plugin.settings.replacementChar)
 				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
+					this.plugin.settings.replacementChar = value;
 					await this.plugin.saveSettings();
 				}));
 	}
